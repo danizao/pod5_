3ebdd75d399427944f6036cba6eff72e98707a0e
danizao <81486851+danizao@users.noreply.github.com>
2023-05-24 21:32:18 +0100

    test to commit

--- life_expectancy/tests/test_loaders.py
+++ life_expectancy/tests/test_loaders.py
@@ -3,6 +3,7 @@ import pandas as pd
 from life_expectancy.loaders import save_data, load_data
 from pytest import MonkeyPatch
 from pathlib import Path
+import os
 
 # BASE_DIR = Path(__file__).parent.parent / "data"
 FIXTURES_DIR = Path(__file__).parent / "fixtures"
@@ -12,20 +13,30 @@ def test_load_data() -> None:
 
     """testing if the load_data is called with the necessary arguments"""
     input = "pt_life_expectancy_expected_test.csv"
-    print("here I am")
     expected = pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_expected.csv")
-    print("Now, here")
     actual = load_data(input)
-    print("and again here")
     pd.testing.assert_frame_equal(actual, expected)
 
-if __name__ == '__main__':
-    test_load_data()
+def test_save_data() -> None:
 
-# def test_save_data(monkeypatch: MonkeyPatch) -> None:
+    """testing if the save_data is called with the necessary arguments"""
+    # Create a sample DataFrame for testing
+    input = {'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']}
+    df = pd.DataFrame(input)
+    
+    # Define the expected file path and name
+    file = "test_data.csv"
+    expected_file_path = os.path.join(FIXTURES_DIR, file)
+    
+    # Call the function to save the data
+    save_data(df, file)
 
-#     """testing if the save_data is called with the necessary arguments"""
+    # Check if the file exists
+    assert os.path.exists(expected_file_path)
 
-#     pass
+    # check if the file content matches the DataFrame
+    loaded_df = pd.read_csv(expected_file_path)
+    assert df.equals(loaded_df)
 
-
+    # Clean up - remove the created file
+    os.remove(expected_file_path)
